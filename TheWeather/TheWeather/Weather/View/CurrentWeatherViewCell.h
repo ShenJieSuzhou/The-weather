e8fa4886478a866d138e2617121b9f5fcc1f6586
ShenJieSuzhou <745689122@qq.com>
2018-01-12 15:22:30 +0800

    完成适配当前天气界面
    完成适配当前天气界面

--- TheWeather/TheWeather/Weather/View/CurrentWeatherViewCell.h
+++ TheWeather/TheWeather/Weather/View/CurrentWeatherViewCell.h
@@ -12,4 +12,42 @@
 
 @property (weak, nonatomic) IBOutlet UIView *quickLookView;
 
+@property (weak, nonatomic) IBOutlet UILabel *tempture;
+
+@property (weak, nonatomic) IBOutlet UILabel *digree;
+
+@property (weak, nonatomic) IBOutlet UILabel *weather;
+
+@property (weak, nonatomic) IBOutlet UILabel *windOritation;
+
+@property (weak, nonatomic) IBOutlet UILabel *windStrength;
+
+@property (weak, nonatomic) IBOutlet UILabel *humidity;
+
+@property (weak, nonatomic) IBOutlet UILabel *time;
+
+@property (weak, nonatomic) IBOutlet UIView *todayVIew;
+
+@property (weak, nonatomic) IBOutlet UIView *tommorrowView;
+
+@property (weak, nonatomic) IBOutlet UILabel *data1;
+
+@property (weak, nonatomic) IBOutlet UILabel *range1;
+
+@property (weak, nonatomic) IBOutlet UILabel *weather1;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icon1;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *data2;
+
+@property (weak, nonatomic) IBOutlet UILabel *range2;
+
+@property (weak, nonatomic) IBOutlet UILabel *weather2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icon2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *seperate;
+
+@property (weak, nonatomic) IBOutlet UIImageView *seperateHor;
 @end
--- TheWeather/TheWeather/Weather/View/CurrentWeatherViewCell.m
+++ TheWeather/TheWeather/Weather/View/CurrentWeatherViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "CurrentWeatherViewCell.h"
+#import "Masonry.h"
 
 @implementation CurrentWeatherViewCell
 
@@ -15,6 +16,50 @@
     // Initialization code
 }
 
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    CGRect rect = self.frame;
+    CGFloat base_Y = self.seperateHor.frame.origin.y;
+
+    [self.todayVIew setFrame:CGRectMake(0, base_Y + 1, (rect.size.width -  1) / 2, rect.size.height - base_Y - 1)];
+    [self.tommorrowView setFrame:CGRectMake((rect.size.width -  1) / 2 + 1, base_Y + 1, (rect.size.width -  1) / 2, rect.size.height - base_Y - 1)];
+    [self.seperate setFrame:CGRectMake((rect.size.width -  1) / 2, base_Y + 1, 1, rect.size.height - base_Y - 1)];
+    
+    [self.data1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_todayVIew).with.offset(20);
+        make.left.equalTo(_todayVIew).with.offset(20);
+    }];
+    
+    [self.range1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_todayVIew).with.offset(20);
+        make.right.equalTo(_todayVIew).with.offset(-20);
+    }];
+    
+    [self.weather1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_data1).with.offset(40);
+        make.left.equalTo(_todayVIew).with.offset(20);
+    }];
+    
+    [self.data2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_tommorrowView).with.offset(20);
+        make.left.equalTo(_tommorrowView).with.offset(20);
+    }];
+    
+    [self.range2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_tommorrowView).with.offset(20);
+        make.right.equalTo(_tommorrowView).with.offset(-20);
+    }];
+    
+    [self.weather2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_data2).with.offset(40);
+        make.left.equalTo(_tommorrowView).with.offset(20);
+    }];
+ 
+    [self.icon1 setFrame:CGRectMake(self.todayVIew.frame.size.width - 20 - self.icon1.frame.size.width, self.weather1.frame.origin.y, self.icon1.frame.size.width, self.icon1.frame.size.height)];
+    
+    [self.icon2 setFrame:CGRectMake(_tommorrowView.frame.size.width - 20 - self.icon2.frame.size.width, self.weather2.frame.origin.y, self.icon2.frame.size.width, self.icon2.frame.size.height)];
+    
+}
 
 
 @end
